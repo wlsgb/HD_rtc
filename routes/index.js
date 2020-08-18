7b26edd25043f8f4d76bbdf12672abbbc195ebf3
=wlsgb <=wlsgb94@naver.com>
2020-08-18 10:17:07 +0900

    상담시작 종료 유아이 변경

--- routes/index.js
+++ routes/index.js
@@ -21,35 +21,34 @@ oracledb.getConnection(config,(err,con)=>{
 oracledb.autoCommit=true;
 
  /* Get home page*/
-function todoWork(err,connection){
-    if(err){
-      console.error(err.message);
-      return;
-    }
-}
- router.get('/chatClient/',function(req,res){ // GET방식이며 '/'로 시작한다면 여기서 처리
    var resultData = [];
-   conn.execute("select cl_code, mem_code, cl_name, cl_url, cl_result, cl_room, to_char(cl_date,'YYYY\"년\"MM\"월\"') as cl_date from chatlog order by cl_code DESC",[],function(err,result){
+ router.get('/chatClient/',function(req,res){ // GET방식이며 '/'로 시작한다면 여기서 처리
+
+   conn.execute("select cl_code, mem_code, cl_name, cl_url, cl_result, cl_room, to_char(cl_date,'yyyy/mm/dd \n hh24:mi:ss') as cl_date from chatlog order by cl_code DESC",[],function(err,result){
      if(err){
        console.error(err.message);
-       // doRelease(result);
        return;
      }
      console.log("typeof(resultData): "+typeof(resultData)) // 테이블 스키마
      console.log("resultData[0]: "+resultData[0]) // 데이터
      resultData = result.rows;
-     // doRelease(result);
      res.render("index",{
        resultData:resultData
      })
    });
  });
- function doRelease(connection){
-   connection.release(function (err){
+
+ router.get('/chatEnd/',function(req,res){ // GET방식이며 '/'로 시작한다면 여기서 처리
+   var query = "update chatlog set cl_result='END' where cl_code="+req.query.cl_code;
+   conn.execute(query,function(err,result){
      if(err){
        console.error(err.message);
+       return;
      }
-   });
- }
+     res.redirect('/chatClient');
+
+   })
+ });
+
 
 module.exports = router; // module을 require 할 때 반환되는 것은 router이다.
